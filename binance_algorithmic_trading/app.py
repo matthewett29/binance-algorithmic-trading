5147c5b09e428177f43c605f74fd7c9bc4ee8ddf
Matt Hewett <matthewett29@gmail.com>
2024-01-08 19:35:52 +1100

    feat: add stats as dataframe and log to html

--- binance_algorithmic_trading/app.py
+++ binance_algorithmic_trading/app.py
@@ -19,15 +19,15 @@ def main():
     # Get config options
     config = get_config()
 
-    # Initialise binance API client manager and connect
-    # using the API key/secret from app.cfg
-    client_manager = ClientManager(
-        log_level=logging.DEBUG,
-        config=config['binance']
-    )
+    # # Initialise binance API client manager and connect
+    # # using the API key/secret from app.cfg
+    # client_manager = ClientManager(
+    #     log_level=logging.DEBUG,
+    #     config=config['binance']
+    # )
 
-    # Get binance exchange info
-    client_manager.get_exchange_info()
+    # # Get binance exchange info
+    # client_manager.get_exchange_info()
 
     # Initialise database
     database_manager = DatabaseManager(
@@ -35,38 +35,39 @@ def main():
         config=config['database']
     )
 
-    # Update database with all new data from binance
-    # for the symbols and intervals enabled in the config file
-    database_manager.update_klines(
-        symbols=config['binance']['SYMBOLS'],
-        intervals=config['binance']['INTERVALS'],
-        client_manager=client_manager
-    )
+    # # Update database with all new data from binance
+    # # for the symbols and intervals enabled in the config file
+    # database_manager.update_klines(
+    #     symbols=config['binance']['SYMBOLS'],
+    #     intervals=config['binance']['INTERVALS'],
+    #     client_manager=client_manager
+    # )
 
     # Initialise Jump In The Flow Strategy
-    strategy = JumpInTheFlowStrategy(
-        log_level=logging.DEBUG,
-        N=15,
-        X=0.1,
-        Y=2.0
-    )
+    strategy = JumpInTheFlowStrategy(log_level=logging.DEBUG)
 
-    #end_time = datetime.strptime("2023-12-17 09:54:00", "%Y-%m-%d %H:%M:%S")
-    end_time= datetime.now()
+    end_time = datetime.strptime("2024-01-08 09:35:00", "%Y-%m-%d %H:%M:%S")
+    # end_time= datetime.now()
 
     # Backtest the strategy on all symols in app.cfg
     strategy.backtest(
         starting_capital=10000,
         risk_percentage=1,
         symbols=config['binance']['SYMBOLS'],
-        start_time=end_time-timedelta(days=20),
+        start_time=end_time-timedelta(days=1825),
         end_time=end_time,
+        entry_interval='12h',
+        trade_interval='30m',
+        N=10,
+        X=0.2,
+        Y=0.55,
+        use_BNB_for_commission=True,
         database_manager=database_manager
     )
 
-    # Print the strategy performance results
-    strategy.calculate_performance()
-    strategy.print_performance()
+    # Get the strategy backtest stats
+    stats = strategy.get_stats()
+    stats.to_html('data/backtest_stats.html')
 
 
 if __name__ == "__main__":
